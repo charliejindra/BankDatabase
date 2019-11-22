19ccf7f2eca502ae0e28f6ab78663482251d7be7
Brian Scalfano <bscalfan@kent.edu>
2019-11-22 13:36:52 -0500

    Testing (Fixed dbconnect)

--- PHPMySQL/dbconnect.php
+++ PHPMySQL/dbconnect.php
@@ -2,8 +2,8 @@
 //Step1
 $servername = "localhost";  // if you run on local server the name is "localhost:3306". If you run on cs server, use only "localhost"
 $username = "root";
-$password = "root";
-$dbname = "university";
+$password = "";
+$dbname = "bank";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
